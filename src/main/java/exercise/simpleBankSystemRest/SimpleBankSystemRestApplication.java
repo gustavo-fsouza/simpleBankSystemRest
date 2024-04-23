1d48479ee847261d47b7eefde71e5cfccf6f9ae3
Gustavo de Souza <gustavo.fsouza@hotmail.com>
2024-04-23 09:07:25 -0300

    first commit

---
+++ src/main/java/exercise/simpleBankSystemRest/SimpleBankSystemRestApplication.java
@@ -0,0 +1,13 @@
+package exercise.simpleBankSystemRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankSystemRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBankSystemRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exercise/simpleBankSystemRest/SimpleBankSystemRestApplicationTests.java
@@ -0,0 +1,13 @@
+package exercise.simpleBankSystemRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankSystemRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
